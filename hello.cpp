87ddfb6b215a49f6120964cbe1bd6b73f841e4ee
diabloyqm <yqm2948@163.com>
2023-09-20 20:01:40 -0400

    second commit

--- hello.cpp
+++ hello.cpp
@@ -4,13 +4,8 @@ using std::cout;
 using std::endl;
 int main()
 {
-    int sum =0 , val = 1;
-    while (val<=10){
-        sum +=val;
-        ++val;
 
-    }
-    std::cout << "Sum of is "<<sum <<std::endl;
+
     return 0;
 
 }
